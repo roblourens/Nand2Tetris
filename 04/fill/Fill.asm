b32d2e427b1af0354ee1727e612d30ac15bba187
Rob Lourens <roblourens@gmail.com>
2017-09-17 19:10:49 -0700

    Project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,59 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// addr=screen
+//
+
+(LOOP)
+
+@KBD
+D=M
+@CLEAR
+D;JEQ // if KBD == 0, goto LOOP; else, draw
+
+D=-1 // Fill(-1)
+(FILL_D)
+@drawValue
+M=D
+
+@8192 // count=8192
+D=A
+@count
+M=D
+
+@SCREEN // addr=SCREEN
+D=A
+@addr
+M=D
+
+(DRAWLOOP)
+@drawValue
+D=M
+@addr
+A=M // A = addr
+M=D // Draw @ addr
+
+@count // count--
+M=M-1
+D=M
+@LOOP // if count==0, goto LOOP
+D;JEQ
+
+@addr // addr += 1
+M=M+1
+
+@DRAWLOOP
+0;JMP
+
+@LOOP
+0;JMP
+
+(CLEAR) // Fill(0)
+D=0
+@FILL_D
+0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,40 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// x = R0
+// y = R1
+// m = 0
+// while (x > 0)
+//   m = m + y
+//   x--
+
+@R0
+D=M
+@count
+M=D
+
+@R2
+M=0
+
+(LOOP) // Loop over all count
+@count // D=R0
+D=M
+
+@END
+D;JEQ // jump to end if finished
+
+@R1
+D=M // D=R1
+
+@R2
+M=D+M
+
+@count
+M=M-1 // R0--
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
